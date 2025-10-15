9f580fd43211562359ce5811c92d2eb26cd93840
Anmol Arora <anmolkdeneon@gmail.com>
2025-10-16 02:12:16 +0530

    graceful shutdown mechanism added

--- server.js
+++ server.js
@@ -2,15 +2,52 @@ const app = require("./app");
 const sequelize = require("./database/sequelize");
 const { PORT } = require("./config/constants");
 
+let server;
+
 (async () => {
   try {
     await sequelize.sync({ alter: true });
     console.log("DB synced");
 
-    app.listen(PORT, () => {
+    server = app.listen(PORT, () => {
       console.log(`Server running at http://localhost:${PORT}`);
+    });
+
+    process.on("SIGINT", gracefulShutdown);
+    process.on("SIGTERM", gracefulShutdown);
+    process.on("uncaughtException", (err) => {
+      console.error("Uncaught Exception:", err);
+      gracefulShutdown();
+    });
+    process.on("unhandledRejection", (reason, promise) => {
+      console.error("Unhandled Rejection:", reason);
+      gracefulShutdown();
     });
   } catch (err) {
     console.log(`Error starting server: ${err}`);
   }
 })();
+
+async function gracefulShutdown() {
+  console.log("\n Starting graceful shutdown...");
+
+  if (server) {
+    // Stop accepting new requests
+    await new Promise((resolve) => {
+      server.close(() => {
+        console.log("HTTP server closed");
+        resolve();
+      });
+    });
+  }
+
+  try {
+    await sequelize.close();
+    console.log("Database connection closed.");
+  } catch (err) {
+    console.error("Error closing database connection:", err);
+  }
+
+  console.log("Exiting process now.");
+  process.exit(0);
+}
